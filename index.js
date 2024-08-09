bd254da3bbb5a3152d784a8dd1e859d8bb1721f2
popelkin-roman <mymaythe23@gmail.com>
2024-08-09 21:14:18 +0500

    Add redirection logic

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dns = require('node:dns');
 const app = express();
+const bodyParser = require('body-parser');
+
+let shortUrls = ['www.ya.ru'];
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -17,6 +21,36 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.use(
+  bodyParser.urlencoded({extended: false})
+)
+
+app.post('/api/shorturl', function(req,res) {
+  let fullUrl = req.body.url;
+  let host = fullUrl.split('://')[1] || fullUrl.split('://')[0];
+  host = host.split('/')[0];
+  dns.lookup(host, (err, address) => {
+    if (err) {
+      return res.json({ error: 'invalid url' })
+    }
+    let shortUrl = shortUrls.findIndex((el) => el === fullUrl);
+    if (shortUrl < 0) {
+      shortUrls.push(fullUrl);
+      shortUrl = shortUrls.length - 1;
+    }
+    app.get(`/api/shorturl/${shortUrl}`, function(req, res) {
+      res.redirect(fullUrl);
+    })
+    res.json({ original_url : fullUrl, short_url : shortUrl})
+  });
+})
+
+shortUrls.forEach( (el, i) => {
+  app.get(`/api/shorturl/${i}`, function(req, res) {
+    res.redirect(`//${el}`);
+  })
 });
 
 app.listen(port, function() {
