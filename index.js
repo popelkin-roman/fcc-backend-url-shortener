37bfba2d4519b9c68871f40992e854a7f8b247be
popelkin-roman <mymaythe23@gmail.com>
2024-08-10 14:48:26 +0500

    Fix: fix test fail

--- index.js
+++ index.js
@@ -27,26 +27,46 @@ app.use(
   bodyParser.urlencoded({extended: false})
 )
 
+// app.post('/api/shorturl', function(req,res) {
+//   let fullUrl = req.body.url;
+//   if (fullUrl[fullUrl.length -1] == "/") fullUrl = fullUrl.substr(0, fullUrl.length - 1);
+//   let urlWithoutProtocol = fullUrl.split('://')[1] || fullUrl;
+//   let host = urlWithoutProtocol.split('/')[0];
+//   dns.lookup(host, (err, address) => {
+//     if (err) {
+//       return res.json({ error: 'invalid url' })
+//     }
+//     let shortUrl = shortUrls.findIndex((el) => el === urlWithoutProtocol);
+//     if (shortUrl < 0) {
+//       shortUrls.push(urlWithoutProtocol);
+//       shortUrl = shortUrls.length - 1;
+//     }
+//     app.get(`/api/shorturl/${shortUrl}`, function(req, res) {
+//       res.redirect(`//${urlWithoutProtocol}`);
+//     })
+//     res.json({ original_url : fullUrl, short_url : shortUrl})
+//   });
+// })
+
 app.post('/api/shorturl', function(req,res) {
   let fullUrl = req.body.url;
   if (fullUrl[fullUrl.length -1] == "/") fullUrl = fullUrl.substr(0, fullUrl.length - 1);
   let urlWithoutProtocol = fullUrl.split('://')[1] || fullUrl;
   let host = urlWithoutProtocol.split('/')[0];
-  dns.lookup(host, (err, address) => {
-    if (err) {
-      return res.json({ error: 'invalid url' })
-    }
-    let shortUrl = shortUrls.findIndex((el) => el === urlWithoutProtocol);
-    if (shortUrl < 0) {
-      shortUrls.push(urlWithoutProtocol);
-      shortUrl = shortUrls.length - 1;
-    }
-    app.get(`/api/shorturl/${shortUrl}`, function(req, res) {
-      res.redirect(`//${urlWithoutProtocol}`);
-    })
-    res.json({ original_url : fullUrl, short_url : shortUrl})
-  });
+  let shortUrl = shortUrls.findIndex((el) => el === urlWithoutProtocol);
+  if (shortUrl < 0) {
+    shortUrls.push(urlWithoutProtocol);
+    shortUrl = shortUrls.length - 1;
+  }
+  app.get(`/api/shorturl/${shortUrl}`, function(req, res) {
+    res.redirect(`//${urlWithoutProtocol}`);
+  })
+  if (urlWithoutProtocol.includes(':/')) return res.json({ error: 'invalid url' })
+  res.json({ original_url : fullUrl, short_url : shortUrl})
+
 })
+
+
 
 shortUrls.forEach( (el, i) => {
   app.get(`/api/shorturl/${i}`, function(req, res) {
